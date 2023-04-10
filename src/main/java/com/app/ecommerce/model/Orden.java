a513065aa6097a74a812fc5524f7d12cb99b6c7f
Daniel Valenzuela <hetzondaniel1@gmail.com>
2023-04-09 23:19:27 -0600

    Se agrego el bootStrap

--- src/main/java/com/app/ecommerce/model/Orden.java
+++ src/main/java/com/app/ecommerce/model/Orden.java
@@ -2,19 +2,34 @@ package com.app.ecommerce.model;
 
 import java.sql.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ordenes")
 public class Orden {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String numero;
 	private Date fechaCreacion;
 	private Date fechaRecibida;
-	
+
 	private Double total;
-	
+	@ManyToOne
+	private Usuario usuario;
+	@OneToOne(mappedBy = "orden")
+	private DetalleOrden detalle;
+
 	
 	public Orden() {
 		// TODO Auto-generated constructor stub
 	}
-
 
 	public Orden(Integer id, String numero, Date fechaCreacion, Date fechaRecibida, Double total) {
 		super();
@@ -25,62 +40,66 @@ public class Orden {
 		this.total = total;
 	}
 
-
 	public Integer getId() {
 		return id;
 	}
-
 
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
-
 	public String getNumero() {
 		return numero;
 	}
-
 
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
 
-
 	public Date getFechaCreacion() {
 		return fechaCreacion;
 	}
-
 
 	public void setFechaCreacion(Date fechaCreacion) {
 		this.fechaCreacion = fechaCreacion;
 	}
 
-
 	public Date getFechaRecibida() {
 		return fechaRecibida;
 	}
-
 
 	public void setFechaRecibida(Date fechaRecibida) {
 		this.fechaRecibida = fechaRecibida;
 	}
 
-
 	public Double getTotal() {
 		return total;
 	}
-
 
 	public void setTotal(Double total) {
 		this.total = total;
 	}
 
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public DetalleOrden getDetalle() {
+		return detalle;
+	}
+
+	public void setDetalle(DetalleOrden detalle) {
+		this.detalle = detalle;
+	}
 
 	@Override
 	public String toString() {
 		return "Orden [id=" + id + ", numero=" + numero + ", fechaCreacion=" + fechaCreacion + ", fechaRecibida="
 				+ fechaRecibida + ", total=" + total + "]";
 	}
-	
-	
+
 }

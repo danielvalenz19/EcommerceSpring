5352ecea275255c91df081dbb84bf553221c296f
Daniel Valenzuela <hetzondaniel1@gmail.com>
2023-04-13 11:37:37 -0600

    The last part of the save was added

--- src/main/java/com/app/ecommerce/controller/ProductoController.java
+++ src/main/java/com/app/ecommerce/controller/ProductoController.java
@@ -1,12 +1,15 @@
 package com.app.ecommerce.controller;
 
 import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.app.ecommerce.model.Producto;
+import com.app.ecommerce.model.Usuario;
+import com.app.ecommerce.service.ProductoService;
 
 import ch.qos.logback.classic.Logger;
 
@@ -14,7 +17,8 @@ import ch.qos.logback.classic.Logger;
 @RequestMapping("/productos")
 public class ProductoController {
 	private final Logger LOGGER = (Logger) LoggerFactory.getLogger(ProductoController.class);
-	
+	@Autowired
+	private ProductoService productoService; 
 	@GetMapping("")
 	public String show() {
 		return "productos/show";
@@ -26,6 +30,9 @@ public class ProductoController {
 	@PostMapping("/save")
 	public String save(Producto producto) {
 		LOGGER.info("este es el objeto producto {}",producto);
+		Usuario u=new Usuario(1, "", "", "", "", "", "", "");
+		producto.setUsuario(u);
+		productoService.save(producto);
 		return "redirect:/productos";
 	}
 	

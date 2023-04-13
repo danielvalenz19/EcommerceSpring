25f98372f3e10aad1a8df6e10606a1c87a63459d
Daniel Valenzuela <hetzondaniel1@gmail.com>
2023-04-13 10:35:49 -0600

    The first part of the save method is added and the front of adding products is added

--- src/main/java/com/app/ecommerce/controller/ProductoController.java
+++ src/main/java/com/app/ecommerce/controller/ProductoController.java
@@ -1,15 +1,33 @@
 package com.app.ecommerce.controller;
 
+import org.slf4j.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.app.ecommerce.model.Producto;
+
+import ch.qos.logback.classic.Logger;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
+	private final Logger LOGGER = (Logger) LoggerFactory.getLogger(ProductoController.class);
+	
 	@GetMapping("")
 	public String show() {
 		return "productos/show";
 	}
-
+	@GetMapping("/create")
+	public String create() {
+		return"productos/create";
+	}
+	@PostMapping("/save")
+	public String save(Producto producto) {
+		LOGGER.info("este es el objeto producto {}",producto);
+		return "redirect:/productos";
+	}
+	
+	
 }

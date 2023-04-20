ad59db8a9c647e0427d11fe5dc06967d961f9643
Daniel Valenzuela <hetzondaniel1@gmail.com>
2023-04-19 23:14:42 -0600

    display the product view to the user

--- src/main/java/com/app/ecommerce/controller/HomeController.java
+++ src/main/java/com/app/ecommerce/controller/HomeController.java
@@ -1,16 +1,23 @@
 package com.app.ecommerce.controller;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.app.ecommerce.service.ProductoService;
 
+import ch.qos.logback.classic.Logger;
+
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private final Logger log = (Logger) LoggerFactory.getLogger(HomeController.class);//21 6:04 me pide casteo
+	
 	@Autowired
 	private ProductoService productoService;
 
@@ -21,4 +28,10 @@ public class HomeController {
 
 		return "usuario/home";
 	}
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id) {
+		log.info("id producto enviado como parametro {}", id);
+		return "usuario/productohome";
+	}
+
 }
